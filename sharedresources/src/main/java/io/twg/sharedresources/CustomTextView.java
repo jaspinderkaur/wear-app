1285a03c8cc73aa5bff29fd2c0b0320083fae485
Jaspinder <Jaspinder@SohiBalbir-MacBook-Air.local>
2017-02-27 14:49:54 +0530

    remove attributes for typeface change

--- sharedresources/src/main/java/io/twg/sharedresources/CustomTextView.java
+++ sharedresources/src/main/java/io/twg/sharedresources/CustomTextView.java
@@ -1,7 +1,6 @@
 package io.twg.sharedresources;
 
 import android.content.Context;
-import android.content.res.TypedArray;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.widget.TextView;
@@ -10,27 +9,21 @@ public class CustomTextView extends TextView {
 
     public CustomTextView(Context context) {
         super(context);
-        onInitTypeface(context, null, CustomFontManager.FONT_POIRET);
+        onInitTypeface(context);
     }
 
     public CustomTextView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        onInitTypeface(context, attrs, CustomFontManager.FONT_POIRET);
+        onInitTypeface(context);
     }
 
     public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        onInitTypeface(context, attrs, CustomFontManager.FONT_POIRET);
+        onInitTypeface(context);
     }
 
-    private void onInitTypeface(Context context, AttributeSet attrs, int defStyle) {
-        int typefaceValue = CustomFontManager.FONT_POIRET;
-        if (attrs != null) {
-            TypedArray values = context.obtainStyledAttributes(attrs, R.styleable.CustomTextView, defStyle, 0);
-            typefaceValue = values.getInt(R.styleable.CustomTextView_font, CustomFontManager.FONT_POIRET);
-            values.recycle();
-        }
-        Typeface customTypeFace = CustomFontManager.obtainTypeface(context, typefaceValue);
+    private void onInitTypeface(Context context) {
+        Typeface customTypeFace = Typeface.createFromAsset(context.getAssets(), "fonts/PoiretOne-Regular.ttf");
         setTypeface(customTypeFace);
     }
 }
